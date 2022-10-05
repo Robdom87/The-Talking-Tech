ed712b836976e1bc384ed3cb8f38777b45225f96
Cedar Chavez <geggas00@gmail.com>
2022-10-05 12:45:46 -0700

    set up server.js for all packages

--- server.js
+++ server.js
@@ -1,20 +1,44 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes_example');
-// import sequelize connection
+//pull in session packages for express and handlebars
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 //if force true, will drop everything
-sequelize.sync().then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
-      console.log(`App listening on port ${PORT}!`);
+      console.log(`App listening on http://localhost:${PORT}/`);
     });
   });
